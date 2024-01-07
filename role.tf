53fa99ba45debe017d06dc530a3eba20eaa9abe4
Paul <paul.hallam@gmail.com>
2024-01-07 16:29:34 +0000

    Create role.tf

---
+++ role.tf
@@ -0,0 +1,13 @@
+resource "aws_iam_role" "github_actions_role" {
+  name = "GitHubActionsRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      }
+    }]
+  })
+}
