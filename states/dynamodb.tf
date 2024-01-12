6f0492b0b8be9692b28f5f2da7507b9026f819de
Paul <paul.hallam@gmail.com>
2024-01-12 17:28:31 +0000

    Create dynamodb.tf

---
+++ states/dynamodb.tf
@@ -0,0 +1,27 @@
+#---------------------------------------------------------------------------------------------------
+# DynamoDB Table for State Locking
+#---------------------------------------------------------------------------------------------------
+
+locals {
+  # The table must have a primary key named LockID.
+  # See https://www.terraform.io/docs/backends/types/s3.html#dynamodb_table
+  lock_key_id = "LockID"
+}
+
+resource "aws_dynamodb_table" "lock" {
+  name                        = var.dynamodb_table_name
+  billing_mode                = var.dynamodb_table_billing_mode
+  hash_key                    = local.lock_key_id
+  deletion_protection_enabled = var.dynamodb_deletion_protection_enabled
+
+  attribute {
+    name = local.lock_key_id
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  tags = var.tags
+}
