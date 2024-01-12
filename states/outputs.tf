459e1ed9561bc6a38c648c485e858cb66ceb8f5f
Paul <paul.hallam@gmail.com>
2024-01-12 17:51:45 +0000

    Update outputs.tf

--- states/outputs.tf
+++ states/outputs.tf
@@ -10,5 +10,5 @@ output "dynamodb_table" {
 
 output "terraform_iam_policy" {
   description = "The IAM Policy to access remote state environment."
-  value       = var.terraform_iam_policy_create ? aws_iam_policy.terraform[0] : null
+  value       = aws_iam_policy.terraform
 }
