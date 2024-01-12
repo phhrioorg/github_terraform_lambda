b62c0a5257df05f8fdc071f2f5cef7ce00697dfe
Paul <paul.hallam@gmail.com>
2024-01-12 16:56:49 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,15 @@
-data "terraform_remote_state" "master_state" {
-  backend = "s3"
-  config = {
-    bucket = "${var.csp_tf_s3_bucket}"
-    key = "${var.csp_tf_state_key}"
-    region = "${var.csp_region}"
+terraform {
+  backend "s3" {
+    bucket = "csp-tf-state-bucket20240112"
+    key    = "aws_dynamodb_table.hash_key"
   }
 }
+
+#data "terraform_remote_state" "master_state" {
+#  backend = "s3"
+#  config = {
+#    bucket = "${var.csp_tf_s3_bucket}"
+#    key = "${var.csp_tf_state_key}"
+#    region = "${var.csp_region}"
+#  }
+#}
