2621a65a16795bf91e25121d4dc9d662575738f8
Paul Hallam <paul.hallam@gmail.com>
2024-01-19 15:31:46 +0000

    test

--- backend.tf
+++ backend.tf
@@ -1,7 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "phh-tf-state-bucket"
-    key    = "aws_dynamodb_table.hash_key"
+#    bucket = "phh-tf-state-bucket"
+#    key    = "aws_dynamodb_table.hash_key"
+    key            = "PSA_state_files/cohort/accounts/tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "iphh-tf-state-lock"
+    encrypt        = true
   }
 }
 
