378a5655bff770fee5773e646da6665b6e076592
Paul <paul.hallam@gmail.com>
2024-01-19 15:00:21 +0000

    Update ecr.tf

--- module/ecr/ecr.tf
+++ module/ecr/ecr.tf
@@ -1,4 +1,4 @@
-resource aws_ecr_repository UK-CoPPS {
+resource "aws_ecr_repository" "UK-CoPPS" {
   for_each             = toset (var.ecr_name)
   name                 = each.key
   image_tag_mutability = var.image_mutability 
