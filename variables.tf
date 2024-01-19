f6165d9897c154191513de217587acb3bf31f298
Paul <paul.hallam@gmail.com>
2024-01-19 13:43:20 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,25 @@ variable "csp_tf_state_key" {
   type    = string
   default = "LockID"
 }
+
+# AFTER ECR
+variable "ecr_name" {
+  description = "The list of ECR names to create" 
+  type = list(string)
+  default = null
+}
+variable "image_mutability" {
+  description = "Image mutability on or off" 
+  type = string
+  default = "MUTABLE"
+}
+variable "encrypt_type" {
+  description = "Encryption type" 
+  type = string
+  default = "KMS"
+}
+variable "tags" {
+  description = "tag mapping" 
+  type = map(string)
+  default = {}
+}
